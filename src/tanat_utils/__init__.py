2588f61da432f75a8669064ca35eabf0e63d8999
Arnaud Duvermy <arnaud.duvermy@inria.fr>
2026-03-06 18:49:46 +0100

    feat: include DisplayIndentManager and DisplayMixin in module exports

--- src/tanat_utils/__init__.py
+++ src/tanat_utils/__init__.py
@@ -5,6 +5,7 @@ tanat-utils: Utilities for TanaT.
 
 #
 from .caching import settings_dataclass, Cachable, CachableSettings, fingerprint
+from .display import DisplayIndentManager, DisplayMixin
 from .registrable import Registrable
 
 __all__ = [
@@ -12,5 +13,7 @@ __all__ = [
     "Cachable",
     "CachableSettings",
     "fingerprint",
+    "DisplayIndentManager",
+    "DisplayMixin",
     "Registrable",
 ]
