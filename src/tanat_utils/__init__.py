03476a9eec7d3cd85ea77e02de9a80779a180edb
Arnaud Duvermy <arnaud.duvermy@inria.fr>
2026-04-09 11:54:57 +0200

    fix: reorganize imports and update __all__ exports to include SettingsMixin

--- src/tanat_utils/__init__.py
+++ src/tanat_utils/__init__.py
@@ -4,13 +4,20 @@ tanat-utils: Utilities for TanaT.
 """
 
 #
-from .caching import settings_dataclass, Cachable, CachableSettings, fingerprint
+from .caching import (
+    settings_dataclass,
+    Cachable,
+    SettingsMixin,
+    CachableSettings,
+    fingerprint,
+)
 from .display import DisplayIndentManager, DisplayMixin
 from .registrable import Registrable
 
 __all__ = [
     "settings_dataclass",
     "Cachable",
+    "SettingsMixin",
     "CachableSettings",
     "fingerprint",
     "DisplayIndentManager",
